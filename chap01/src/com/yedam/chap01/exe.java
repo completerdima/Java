fa4aa868a5a865ebbcd8d18d2158156173facb36
admin <admin@YD02-17>
2023-05-08 15:29:51 +0900

    test2 commit

---
+++ chap01/src/com/yedam/chap01/exe.java
@@ -0,0 +1,13 @@
+package com.yedam.chap01;
+
+public class exe {
+
+	public static void main (String[] arg) {
+		System.out.println("Hello world");
+		//주석 ->한줄 주석
+		/*
+		 * 
+		 */
+	//실행 ->주석, calss 포함 x -> 실행(주석빼고)
+	}
+}
